db19d2d79bec9512df3a262c8dd41ead7730c21f
WiktorMarcin <w-marcinkowski@wp.pl>
2024-10-27 22:57:44 +0100

    Added gallery of photos using SwiperJS

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 window.addEventListener("scroll", () => {document.querySelector("nav").classList.toggle("scrolled", window.scrollY > 0)})
+
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
